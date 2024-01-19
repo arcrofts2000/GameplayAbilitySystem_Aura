0a5808c7561bd3862c9e097e5735a5dfaffb3d90
arcrofts2000 <arcrofts2000@gmail.com>
2024-01-19 17:12:13 +0000

    Added Mana and Health Attributes

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,33 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "VitalAttributes")
+	FGameplayAttributeData Health;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "VitalAttributes")
+	FGameplayAttributeData MaxHealth;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "VitalAttributes")
+	FGameplayAttributeData Mana;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "VitalAttributes")
+	FGameplayAttributeData MaxMana;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
+
+protected:
+
+
+private:
+
 };
